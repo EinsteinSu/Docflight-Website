54f513ad2c39ed0d32f8f6b90ba0eebbbe12c00a
Sue Su <einstein_supeng@hotmail.com>
2017-01-15 20:46:10 +0800

    the first version

---
+++ js/ui-responsive.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var x = document.getElementById("topnav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
