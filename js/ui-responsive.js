2b740707b6300775d21206569bda0559ba149076
Sue Su <einstein_supeng@hotmail.com>
2017-01-19 12:25:18 +0800

    refer bootstrap into project.

--- js/ui-responsive.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    var x = document.getElementById("topnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
